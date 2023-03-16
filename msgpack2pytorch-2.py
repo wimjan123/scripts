5ea07881b92b1e2e88e4715a08d47ed88c5ea208
wimjan123 <visser.william@live.nl>
2023-03-16 21:18:11 +0100

    Update msgpack2pytorch-2.py

--- msgpack2pytorch-2.py
+++ msgpack2pytorch-2.py
@@ -1,10 +1,9 @@
+import os
 import msgpack
-import msgpack_numpy as m
-from transformers import GPTJConfig, FlaxGPTJModel 
+import torch
+from transformers import GPTJForCausalLM, GPTJConfig
 
-m.patch()
-
-def load_streaming_msgpack_file(file_path): 
+def load_streaming_msgpack_file(file_path):
     data = {}
     with open(file_path, "rb") as f:
         unpacker = msgpack.Unpacker(f, max_buffer_size=280 * 1024**3)  # Increase buffer size to 280 GB
@@ -12,19 +11,29 @@ def load_streaming_msgpack_file(file_path):
             data[tuple(key)] = value
     return data
 
-
+def unflatten_dict(dictionary):
+    unflattened = {}
+    for key_tuple, value in dictionary.items():
+        current_level = unflattened
+        for key in key_tuple[:-1]:
+            if key not in current_level:
+                current_level[key] = {}
+            current_level = current_level[key]
+        current_level[key_tuple[-1]] = value
+    return unflattened
 
 def load_flax_model_from_train_state(config_file, train_state):
     config = GPTJConfig.from_json_file(config_file)
-    flax_model = FlaxGPTJModel(config)
-    flax_model.params = train_state["params"]
+    flax_model = GPTJForCausalLM(config)
+    flax_model.params = unflatten_dict(train_state)
     return flax_model
 
 msgpack_file = "/temp/streaming_params"
-config_file = "/temp/config.json"
-
-# Load the msgpack file
 msgpack_data = load_streaming_msgpack_file(msgpack_file)
 
-# Load the Flax model from the train state
+config_file = "/temp/config.json"
 flax_model = load_flax_model_from_train_state(config_file, msgpack_data)
+
+# Convert the Flax model to a PyTorch model and save it
+pytorch_model = GPTJForCausalLM.from_pretrained(flax_model, from_flax=True)
+pytorch_model.save_pretrained("converted_pytorch_model")
