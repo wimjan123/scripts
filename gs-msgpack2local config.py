ab5f4e015f997475ce1f1db7fd3c8c26a8562404
wimjan123 <visser.william@live.nl>
2023-03-16 21:06:17 +0100

    sdf

--- gs-msgpack2local config.py
+++ gs-msgpack2local config.py
@@ -12,6 +12,6 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
 # Replace these with your own values
 bucket_name = 'gpt-j-train'
 source_blob_name = 'GPT-Reddit-output-V2.2-v3/9a2e902d060e4e17b67923b51a356c9c/streaming_params'
-destination_file_name = '/temp/streaming_params'
+destination_file_name = 'flax-weights.msgpack'
 
 download_blob(bucket_name, source_blob_name, destination_file_name)
