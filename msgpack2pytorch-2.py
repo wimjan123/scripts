07ea9892d6c91d87cfa6c2a2f0a019d85cada4fa
wimjan123 <visser.william@live.nl>
2023-03-16 20:47:10 +0100

    Update msgpack2pytorch-2.py

--- msgpack2pytorch-2.py
+++ msgpack2pytorch-2.py
@@ -1,16 +1,15 @@
-from transformers import GPTJConfig, FlaxGPTJModel
+import msgpack
+import msgpack_numpy as m
+from transformers import GPT2Config
+from custom_functions import load_streaming_msgpack_file, load_flax_model_from_train_state
 
-def load_flax_model_from_train_state(config_file, train_state):
-    # Load the model config
-    config = GPTJConfig.from_json_file(config_file)
-    
-    # Create the Flax model
-    flax_model = FlaxGPTJModel(config)
-    
-    # Set the Flax model weights from the train state
-    flax_model.params = train_state.params
+m.patch()
 
-    return flax_model
-
+msgpack_file = "/temp/streaming_params"
 config_file = "/temp/config.json"
+
+# Load the msgpack file
+msgpack_data = load_streaming_msgpack_file(msgpack_file)
+
+# Load the Flax model from the train state
 flax_model = load_flax_model_from_train_state(config_file, msgpack_data)
